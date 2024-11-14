a711d6dba54123b05a4ea62fb4b5ab185e79c4e3
Fábio Delazari Pereira <delazari.rj@gmail.com>
2024-11-14 19:43:24 -0300

    create CardController class

---
+++ src/main/java/com/delazari/java_spring_example/controllers/CardController.java
@@ -0,0 +1,25 @@
+package com.delazari.java_spring_example.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.delazari.java_spring_example.entities.Card;
+import com.delazari.java_spring_example.services.CardService;
+
+@RestController
+@RequestMapping(value = "/cards")
+public class CardController {
+	
+	@Autowired
+	private CardService service;
+	
+	@GetMapping
+	public List<Card> findAll() {
+		return service.findAll();
+	}
+
+}
