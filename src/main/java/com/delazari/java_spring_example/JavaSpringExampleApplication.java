ad3ffa70fc57f9a6589c23e4e9e6d229b0af6636
Fábio Delazari Pereira <delazari.rj@gmail.com>
2024-11-14 18:24:48 -0300

    create a project with h2 dependency

---
+++ src/main/java/com/delazari/java_spring_example/JavaSpringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.delazari.java_spring_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delazari/java_spring_example/JavaSpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delazari.java_spring_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
