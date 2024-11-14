640a0a769737a6c2108bf5addf4407ea52fa4606
Fábio Delazari Pereira <delazari.rj@gmail.com>
2024-11-14 19:26:23 -0300

    seed h2 database with cards and add column annotation in description attribute

--- src/main/java/com/delazari/java_spring_example/entities/Card.java
+++ src/main/java/com/delazari/java_spring_example/entities/Card.java
@@ -2,6 +2,7 @@ package com.delazari.java_spring_example.entities;
 
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ public class Card {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	@Column(columnDefinition = "TEXT")
 	private String description;
 	
 	public Card() {}
